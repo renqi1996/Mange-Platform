102b41b138b2a41b62ce6fb731703220f4d916fd
walle <975965715@qq.com>
2021-01-04 23:26:50 +0800

    feat: update config;

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -58,24 +58,12 @@ const menuDataRender = (menuList: MenuDataItem[]): MenuDataItem[] =>
 
 const defaultFooterDom = (
   <DefaultFooter
-    copyright={`${new Date().getFullYear()} 蚂蚁集团体验技术部出品`}
+    copyright={`${new Date().getFullYear()} Wall-E`}
     links={[
-      {
-        key: 'Ant Design Pro',
-        title: 'Ant Design Pro',
-        href: 'https://pro.ant.design',
-        blankTarget: true,
-      },
       {
         key: 'github',
         title: <GithubOutlined />,
         href: 'https://github.com/ant-design/ant-design-pro',
-        blankTarget: true,
-      },
-      {
-        key: 'Ant Design',
-        title: 'Ant Design',
-        href: 'https://ant.design',
         blankTarget: true,
       },
     ]}
--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,11 +1,10 @@
 import type { MenuDataItem } from '@ant-design/pro-layout';
-import { DefaultFooter, getMenuData, getPageTitle } from '@ant-design/pro-layout';
+import { getMenuData, getPageTitle } from '@ant-design/pro-layout';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import type { ConnectProps } from 'umi';
-import { Link, SelectLang, useIntl, connect, FormattedMessage } from 'umi';
+import { SelectLang, useIntl, connect } from 'umi';
 import React from 'react';
 import type { ConnectState } from '@/models/connect';
-import logo from '../assets/logo.svg';
 import styles from './UserLayout.less';
 
 export type UserLayoutProps = {
@@ -40,28 +39,12 @@ const UserLayout: React.FC<UserLayoutProps> = (props) => {
         <meta name="description" content={title} />
       </Helmet>
 
-      <div className={styles.container}>
+      {/* <div className={styles.container}> */}
+      <div>
         <div className={styles.lang}>
           <SelectLang />
         </div>
-        <div className={styles.content}>
-          <div className={styles.top}>
-            <div className={styles.header}>
-              <Link to="/">
-                <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
-              </Link>
-            </div>
-            <div className={styles.desc}>
-              <FormattedMessage
-                id="pages.layouts.userLayout.title"
-                defaultMessage="Ant Design 是西湖区最具影响力的 Web 设计规范"
-              />
-            </div>
-          </div>
-          {children}
-        </div>
-        <DefaultFooter />
+        <div className={styles.content}>{children}</div>
       </div>
     </HelmetProvider>
   );
